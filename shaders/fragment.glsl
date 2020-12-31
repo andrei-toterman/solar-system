87dc089294d92c8e94a101c2e59a44835d4a0125
iuliusavgvstvs <iulius.satmari@gmail.com>
2021-01-01 00:16:49 +0200

    phong light implementation

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,12 +5,45 @@ in vec2 texture_coords;
 uniform sampler2D sampler;
 
 in vec4 varyingColor;
+
+uniform vec4 globalAmbient;
+uniform vec3 PL_position;
+uniform vec4 PL_ambient;
+uniform vec4 PL_diffuse;
+uniform vec4 PL_specular;
+uniform vec4 M_ambient;
+uniform vec4 M_diffuse;
+uniform vec4 M_specular;
+uniform float M_shininess;
+in vec3 varyingNormal;
+in vec3 varyingLightDir;
+in vec3 varyingVertPos;
+in vec3 _isSun;
+
+
 //in vec2 tc;
 
 void main() {
-    
-    if(varyingColor!=vec4(-1,-1,-1,-1)) // not sun, se aplica ambiental light pe textura
-        color = texture(sampler, texture_coords) * varyingColor;
-    else // sun, texutra ramane la fel = luminoasa
+
+    if(_isSun[0] == 1){
         color = texture(sampler, texture_coords);
+    }
+    else{
+        // normalize the light, normal, and view vectors:
+        vec3 L = normalize(varyingLightDir);
+        vec3 N = normalize(varyingNormal);
+        vec3 V = normalize(-varyingVertPos);
+        // compute light reflection vector with respect to N:
+        vec3 R = normalize(reflect(-L, N));
+        // get the angle between the light and surface normal:
+        float cosTheta = dot(L,N);
+        // angle between the view vector and reflected light:
+        float cosPhi = dot(V,R);
+        // compute ADS contributions (per pixel), and combine to build output color:
+        vec4 texColor = texture(sampler, texture_coords);
+        vec3 ambient = ((globalAmbient * texColor) + (PL_ambient * texColor)).xyz;
+        vec3 diffuse = PL_diffuse.xyz * texColor.xyz * max(cosTheta,0.0);
+        //vec3 specular = PL_specular.xyz * texColor.xyz * pow(max(cosPhi,0.0), M_shininess);
+        color = vec4((ambient*0.2 + diffuse), 1.0);
+    }    
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -9,41 +9,20 @@ uniform vec3 isSun;
 out vec2 texture_coords;
 
 uniform mat4 mv_matrix;
-uniform mat4 proj_matrix;
 uniform mat4 norm_matrix;
-
-uniform vec4 globalAmbient;
-
 uniform vec3 PL_position;
-uniform vec4 PL_ambient;
-uniform vec4 PL_diffuse;
-uniform vec4 PL_specular;
 
-uniform vec4 M_ambient;
-uniform vec4 M_diffuse;
-uniform vec4 M_specular;
-uniform float M_shininess;
-//ambient
-out vec4 varyingColor;
-
-uniform vec3 lightPosition;
+out vec3 _isSun;
+out vec3 varyingNormal; // eye-space vertex normal
+out vec3 varyingLightDir;// vector pointing to the light
+out vec3 varyingVertPos;// vertex position in eye space
 
 void main() {
-    gl_Position = mvp * vec4(position, 1.0);
+    _isSun = isSun;
     texture_coords = _texture_coords;
 
-    if(isSun[0]==0)
-    {   
-        vec3 ambient = ((globalAmbient * M_ambient) + (PL_ambient * M_ambient)).xyz;
-        vec4 P = mv_matrix * vec4(position,1.0);
-        vec3 N = normalize((norm_matrix * vec4(normal_coords,1.0)).xyz);
-        vec3 L = normalize(PL_position - P.xyz);
-        vec3 diffuse = PL_diffuse.xyz * M_diffuse.xyz * max(dot(N,L), 0.0);
-        varyingColor = vec4((ambient + diffuse), 1.0);
-        
-    }
-    else{
-        varyingColor = vec4(-1,-1,-1,-1);
-    }
-    
+    varyingVertPos=(mv_matrix * vec4(position,1.0)).xyz;
+    varyingLightDir = PL_position - varyingVertPos;
+    varyingNormal=(norm_matrix * vec4(normal_coords,1.0)).xyz;
+    gl_Position = mvp * vec4(position, 1.0);    
 }
